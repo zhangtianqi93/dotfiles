56fd92979e6421ebd10f8cad67de24c252f0bd23
张天其 <zhangtianqi@inke.cn>
2023-07-01 21:18:24 +0800

    feat: add rime

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -g fish_greeting
+
+set -xg GOPATH $HOME/Work/code/go
+
+fish_add_path $GOPATH/bin
+fish_add_path $HOME/.local/bin
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
+
+zoxide init fish | source
+
+starship init fish | source
